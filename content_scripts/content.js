6434eb3472a0650720cfcfe876d05b472f270d70
PrashanthaTP <tpprashantha@gmail.com>
2023-07-30 21:46:06 +0530

    Dev: Add edge case to skip last chunk in matched string

--- content_scripts/content.js
+++ content_scripts/content.js
@@ -1,28 +1,33 @@
 const target = document.querySelector("html");
-const targetText = "answer"
-const replaceText = "ATYALAMACHA"
+const targetText = "answer";
+const replaceText = "ATYALAMACHA";
+
 const recur = (elem) => {
     Array.from(elem.childNodes).forEach((e, idx) => {
-
         if (e.nodeType === e.TEXT_NODE) {
-			if ( e.nodeValue.match( targetText ) ) {
-				console.log("MATCHED")
-				//let modifiedText = e.nodeValue.replace(new RegExp(`\\b${targetText}\\b`,"gi"), "GREAT");
-				let splittedString = e.nodeValue.split( targetText )
-				const spanElem = document.createElement( 'span' )
-				console.log(splittedString)
-				for ( let split of splittedString ) {
-					spanElem.appendChild( document.createTextNode( split ) );
-
-					let spanChild = document.createElement( 'span' )
-					spanChild.appendChild( document.createTextNode( replaceText ) )
-					spanChild.style.setProperty("background","yellow")
-					spanElem.appendChild(spanChild)
-				}
-				//elem.style.background = "yellow";
-				console.log(spanElem)
-				elem.replaceChild(spanElem,e)
-			}
+            if (e.nodeValue.match(targetText)) {
+                //console.log("MATCHED");
+                //let modifiedText = e.nodeValue.replace(new RegExp(`\\b${targetText}\\b`,"gi"), "GREAT");
+                let splittedString = e.nodeValue.split(targetText);
+                const spanElem = document.createElement("span");
+                //console.log(splittedString);
+                for (let idx in splittedString) {
+                    let split = splittedString[idx];
+                    spanElem.appendChild(document.createTextNode(split));
+                    if (idx == splittedString.length - 1) {
+                        //example : 'answer'ed and targetText = answer
+                        //answered -> splittedString = ['','ed]
+                        continue;
+                    }
+                    let spanChild = document.createElement("span");
+                    spanChild.appendChild(document.createTextNode(replaceText));
+                    spanChild.style.setProperty("background", "yellow");
+                    spanElem.appendChild(spanChild);
+                }
+                //elem.style.background = "yellow";
+                //console.log(spanElem);
+                elem.replaceChild(spanElem, e);
+            }
         }
         //console.log(e,elem.childNodes.length)
         if (e.nodeType === e.ELEMENT_NODE) {
